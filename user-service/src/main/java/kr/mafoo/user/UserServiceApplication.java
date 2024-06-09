2a1ae2daa4ea24d09bedf8c026c82afab18c6786
송영민 <jakgon@naver.com>
2024-06-09 19:32:23 +0900

    init: add user-service module

---
+++ user-service/src/main/java/kr/mafoo/user/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.mafoo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/kr/mafoo/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mafoo.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
