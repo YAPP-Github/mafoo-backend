0bf4bcabf47ef1921ea95380250329ef91501378
송영민 <jakgon@naver.com>
2024-06-14 20:50:50 +0900

    feat: add tags to apis

--- user-service/src/main/java/kr/mafoo/user/api/AuthApi.java
+++ user-service/src/main/java/kr/mafoo/user/api/AuthApi.java
@@ -1,6 +1,7 @@
 package kr.mafoo.user.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.mafoo.user.controller.dto.request.KakaoLoginRequest;
 import kr.mafoo.user.controller.dto.request.TokenRefreshRequest;
 import kr.mafoo.user.controller.dto.response.LoginResponse;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
+@Tag(name = "인증(로그인) 관련 API", description = "토큰 발행, 로그인 등 API")
 @RequestMapping("/v1/auth")
 public interface AuthApi {
     @Operation(summary = "카카오 로그인", description = "카카오 인가 코드로 로그인(토큰 발행)합니다.")
--- user-service/src/main/java/kr/mafoo/user/api/MeApi.java
+++ user-service/src/main/java/kr/mafoo/user/api/MeApi.java
@@ -1,11 +1,13 @@
 package kr.mafoo.user.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.mafoo.user.controller.dto.response.MemberResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
+@Tag(name = "로그인 사용자 정보 API", description = "현재 토큰 주인의 정보를 다루는 API")
 @RequestMapping("/v1/me")
 public interface MeApi {
     @GetMapping
