e3d278d3ab773fea110924e743ce5595f53b484a
송영민 <jakgon@naver.com>
2024-06-14 20:47:12 +0900

    feat: implement apis

--- user-service/src/main/java/kr/mafoo/user/controller/AuthController.java
+++ user-service/src/main/java/kr/mafoo/user/controller/AuthController.java
@@ -1,7 +1,8 @@
 package kr.mafoo.user.controller;
 
 import kr.mafoo.user.api.AuthApi;
-import kr.mafoo.user.controller.dto.request.LoginRequest;
+import kr.mafoo.user.controller.dto.request.KakaoLoginRequest;
+import kr.mafoo.user.controller.dto.request.TokenRefreshRequest;
 import kr.mafoo.user.controller.dto.response.LoginResponse;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,7 +10,12 @@ import reactor.core.publisher.Mono;
 @RestController
 public class AuthController implements AuthApi {
     @Override
-    public Mono<LoginResponse> login(LoginRequest request) {
-        return Mono.empty();
+    public Mono<LoginResponse> loginWithKakao(KakaoLoginRequest request) {
+        return Mono.just(new LoginResponse("test_access_token", "test_refresh_token"));
+    }
+
+    @Override
+    public Mono<LoginResponse> loginWithRefreshToken(TokenRefreshRequest request) {
+        return Mono.just(new LoginResponse("test_access_token", request.refreshToken()));
     }
 }
