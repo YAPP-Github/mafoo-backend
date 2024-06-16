45ff071bca3ab302af1d4d0bb4f4e76f17570f00
송영민 <jakgon@naver.com>
2024-06-16 18:07:37 +0900

    docs: add swagger description

---
+++ user-service/src/main/java/kr/mafoo/user/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package kr.mafoo.user.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("마푸 백엔드 API 명세 - 유저 서비스")
+                        .version("v1")
+                        .description("사용자 정보 관련 API 명세 목록입니다.")
+                        .contact(new Contact()
+                                .name("깃허브 주소")
+                                .url("https://github.com/YAPP-Github/mafoo-backend"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT"))
+                )
+                .addServersItem(new Server()
+                        .url("https://gateway.mafoo.kr/user/")
+                        .description("프로덕션 서버 유저 서비스 URL")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("Authorization",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("Bearer")
+                                                .bearerFormat("JWT"))
+                                );
+    }
+}
