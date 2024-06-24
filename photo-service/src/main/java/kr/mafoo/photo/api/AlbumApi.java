c241af28aa9be9eda98b3f470c03bcd4d3d19555
ChuYong <jakgon@naver.com>
2024-06-24 12:01:14 +0900

    feat: add RequestMemberId feature

--- photo-service/src/main/java/kr/mafoo/photo/api/AlbumApi.java
+++ photo-service/src/main/java/kr/mafoo/photo/api/AlbumApi.java
@@ -3,6 +3,7 @@ package kr.mafoo.photo.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.mafoo.photo.annotation.RequestMemberId;
 import kr.mafoo.photo.controller.dto.request.AlbumCreateRequest;
 import kr.mafoo.photo.controller.dto.request.AlbumRenameRequest;
 import kr.mafoo.photo.controller.dto.request.AlbumRetypeRequest;
@@ -18,12 +19,14 @@ public interface AlbumApi {
     @Operation(summary = "앨범 조회", description = "앨범 목록을 조회합니다.")
     @GetMapping
     Flux<AlbumResponse> getAlbums(
+            @RequestMemberId
             String memberId
     );
 
     @Operation(summary = "앨범 생성", description = "앨범을 생성합니다.")
     @PostMapping
     Mono<AlbumResponse> createAlbum(
+            @RequestMemberId
             String memberId,
 
             @RequestBody
@@ -33,6 +36,7 @@ public interface AlbumApi {
     @Operation(summary = "앨범 이름 변경", description = "앨범의 이름을 변경합니다.")
     @PatchMapping("/{albumId}/name")
     Mono<AlbumResponse> updateAlbumName(
+            @RequestMemberId
             String memberId,
 
             @Parameter(description = "앨범 ID", example = "test_album_id")
@@ -46,6 +50,7 @@ public interface AlbumApi {
     @Operation(summary = "앨범 타입 변경", description = "앨범의 타입을 변경합니다.")
     @PatchMapping("/{albumId}/type")
     Mono<AlbumResponse> updateAlbumType(
+            @RequestMemberId
             String memberId,
 
             @Parameter(description = "앨범 ID", example = "test_album_id")
@@ -59,6 +64,7 @@ public interface AlbumApi {
     @Operation(summary = "앨범 삭제", description = "앨범을 삭제합니다.")
     @DeleteMapping("/{albumId}")
     Mono<Void> deleteAlbum(
+            @RequestMemberId
             String memberId,
 
             @Parameter(description = "앨범 ID", example = "test_album_id")
---
+++ photo-service/src/main/java/kr/mafoo/photo/config/MemberIdParameterResolver.java
@@ -0,0 +1,21 @@
+package kr.mafoo.photo.config;
+
+import kr.mafoo.photo.annotation.RequestMemberId;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.reactive.BindingContext;
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class MemberIdParameterResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(RequestMemberId.class) != null;
+    }
+
+    @Override
+    public Mono<Object> resolveArgument(MethodParameter parameter, BindingContext bindingContext, ServerWebExchange exchange) {
+        String memberId = exchange.getRequest().getHeaders().getFirst("X-MEMBER-ID");
+        return Mono.justOrEmpty(memberId);
+    }
+}
