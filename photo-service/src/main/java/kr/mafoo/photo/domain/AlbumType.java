964de84d9737dc534c37064dd9e54a94c5535feb
송영민 <jakgon@naver.com>
2024-06-30 19:17:00 +0900

    feat: update album types

--- photo-service/src/main/java/kr/mafoo/photo/domain/AlbumType.java
+++ photo-service/src/main/java/kr/mafoo/photo/domain/AlbumType.java
@@ -1,10 +1,10 @@
 package kr.mafoo.photo.domain;
 
 public enum AlbumType {
-    TYPE_A,
-    TYPE_B,
-    TYPE_C,
-    TYPE_D,
-    TYPE_E,
-    TYPE_F,
+    HEART,
+    FIRE,
+    BASKETBALL,
+    BUILDING,
+    STARFALL,
+    SMILE_FACE
 }
