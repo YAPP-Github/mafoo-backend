f304c9c9114ce02380dabf02233cd5cd9e366310
송영민 <jakgon@naver.com>
2024-07-01 16:57:07 +0900

    feat: hide memberId parameter

--- photo-service/src/main/java/kr/mafoo/photo/annotation/RequestMemberId.java
+++ photo-service/src/main/java/kr/mafoo/photo/annotation/RequestMemberId.java
@@ -1,10 +1,12 @@
 package kr.mafoo.photo.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Parameter(hidden = true)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER, ElementType.TYPE_PARAMETER})
 public @interface RequestMemberId {
