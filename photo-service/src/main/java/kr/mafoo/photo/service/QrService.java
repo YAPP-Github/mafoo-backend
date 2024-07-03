2caf284bd117a93f5621439a2ae473ff84107f78
Gyoungmin Kim <kimkm0419@naver.com>
2024-07-03 23:51:00 +0900

    feat: create qr service (#12)
    * feat: implement getFileAsByte
    * feat: implement '인생네컷' brand qr service

---
+++ photo-service/src/main/java/kr/mafoo/photo/service/QrService.java
@@ -0,0 +1,64 @@
+package kr.mafoo.photo.service;
+
+import kr.mafoo.photo.domain.BrandType;
+import kr.mafoo.photo.exception.PhotoBrandNotExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QrService {
+
+    private final WebClient externalWebClient;
+
+    protected Mono<byte[]> getFileFromQrUrl(String qrUrl) {
+        BrandType brandType = Optional.ofNullable(BrandType.matchBrandType(qrUrl))
+                .orElseThrow(PhotoBrandNotExistsException::new);
+
+        return switch (brandType) {
+            case LIFE_FOUR_CUTS -> getLifeFourCutsFiles(qrUrl);
+            case PHOTOISM -> null;
+            case HARU_FILM -> null;
+            case DONT_LOOK_UP -> null;
+        };
+
+    }
+
+    private Mono<byte[]> getLifeFourCutsFiles(String qrUrl) {
+
+        return externalWebClient
+                .get()
+                .uri(qrUrl)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(response -> {
+                    URI redirectUri = response.getHeaders().getLocation();
+                    if (redirectUri == null) {
+                        throw new RuntimeException("No redirection URL found");
+                    } else {
+                        // TODO : 추후 비디오 URL 추가 예정
+                        // String videoUrl = redirectUri.toString().replace("index.html", "video.mp4");
+
+                        String imageUrl = redirectUri.toString().replace("index.html", "image.jpg");
+                        return getFileAsByte(imageUrl);
+                    }
+                });
+    }
+
+    private Mono<byte[]> getFileAsByte(String url) {
+        return externalWebClient
+                .get()
+                .uri(url)
+                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .retrieve()
+                .bodyToMono(byte[].class);
+    }
+
+
+}
