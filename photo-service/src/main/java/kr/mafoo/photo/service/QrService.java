38ca11ff577b4220fff0dcebf350ec876cbd9760
Gyoungmin Kim <kimkm0419@naver.com>
2024-07-04 01:28:48 +0900

    feat: create getHaruFilmFiles function (#12)

--- photo-service/src/main/java/kr/mafoo/photo/service/QrService.java
+++ photo-service/src/main/java/kr/mafoo/photo/service/QrService.java
@@ -3,6 +3,7 @@ package kr.mafoo.photo.service;
 import kr.mafoo.photo.domain.BrandType;
 import kr.mafoo.photo.exception.PhotoBrandNotExistsException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
 import java.net.URI;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class QrService {
@@ -24,7 +26,7 @@ public class QrService {
         return switch (brandType) {
             case LIFE_FOUR_CUTS -> getLifeFourCutsFiles(qrUrl);
             case PHOTOISM -> null;
-            case HARU_FILM -> null;
+            case HARU_FILM -> getHaruFilmFiles(qrUrl);
             case DONT_LOOK_UP -> null;
         };
 
@@ -40,6 +42,19 @@ public class QrService {
                     String imageUrl = redirectUri.toString().replace("index.html", "image.jpg");
                     return getFileAsByte(imageUrl);
                 });
+    }
+
+    private Mono<byte[]> getHaruFilmFiles(String qrUrl) {
+
+        String albumCode = qrUrl.split("/@")[1];
+
+        String baseUrl = "http://haru6.mx2.co.kr/base_api?command=albumdn&albumCode=";
+        String imageUrl = baseUrl + albumCode + "&type=photo&file_name=output.jpg&max=10&limit=+24 hours";
+
+        // TODO : 추후 비디오 URL 추가 예정
+        // String videoUrl = baseUrl + albumCode + "&type=video&max=10&limit=+24 hours";
+
+        return getFileAsByte(imageUrl);
     }
 
     private Mono<URI> getRedirectUri(String url) {
