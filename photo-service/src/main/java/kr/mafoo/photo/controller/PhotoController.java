21cad0e87009c442d5bcc3b2c2794ed4fdea8e75
Gyoungmin Kim <kimkm0419@naver.com>
2024-07-13 12:40:57 +0900

    feat: create temp. response for create photo api (#24)

--- photo-service/src/main/java/kr/mafoo/photo/controller/PhotoController.java
+++ photo-service/src/main/java/kr/mafoo/photo/controller/PhotoController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static kr.mafoo.photo.domain.BrandType.LIFE_FOUR_CUTS;
+
 @RequiredArgsConstructor
 @RestController
 public class PhotoController implements PhotoApi {
@@ -31,6 +33,12 @@ public class PhotoController implements PhotoApi {
             String memberId,
             PhotoCreateRequest request
     ){
+
+        // HACK : 프엔 응급 요청으로 더미 응답 추가했습니다. - 추후 삭제 예정
+        if (request.qrUrl().equals("https://mafoo.kr/")) {
+            return Mono.just(new PhotoResponse("id", "https://kr.object.ncloudstorage.com/mafoo//24c576bc-60b0-4e43-af9a-0c3311b97f35", LIFE_FOUR_CUTS, null));
+        }
+
         return photoService
                 .createNewPhoto(request.qrUrl(), memberId)
                 .map(PhotoResponse::fromEntity);
