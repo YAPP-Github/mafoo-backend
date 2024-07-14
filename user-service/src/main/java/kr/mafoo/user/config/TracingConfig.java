8a90e0749f1356f453dfa90032c920955f12c054
송영민 <jakgon@naver.com>
2024-07-14 20:33:38 +0900

    feat: ignore tracing for specific routes

---
+++ user-service/src/main/java/kr/mafoo/user/config/TracingConfig.java
@@ -0,0 +1,30 @@
+package kr.mafoo.user.config;
+
+import io.micrometer.observation.ObservationPredicate;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.observation.ServerRequestObservationContext;
+
+@Configuration
+public class TracingConfig {
+    @Bean
+    ObservationPredicate noopServerRequestObservationPredicate() {
+        ObservationPredicate  predicate = (name, context) -> {
+            if(context instanceof ServerRequestObservationContext c) {
+                ServerHttpRequest servletRequest = c.getCarrier();
+                String requestURI = servletRequest.getPath().toString();
+                if(StringUtils.containsAny(requestURI, "actuator", "swagger", "api-docs")) {
+                    return false;
+                }
+            }
+            if (StringUtils.equalsAny(name,"spring.security.filterchains","spring.security.authorizations","spring.security.http.secured.requests")) {
+                return false;
+            }
+            return true;
+        };
+
+        return predicate;
+    }
+}
