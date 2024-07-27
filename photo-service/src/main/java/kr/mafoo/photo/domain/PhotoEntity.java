4b20ec0b0926f1d510c201f5fb8defadfa059578
Gyoungmin Kim <kimkm0419@naver.com>
2024-07-27 12:14:46 +0900

    feat : create updateOwnerMemberId for photo entity

--- photo-service/src/main/java/kr/mafoo/photo/domain/PhotoEntity.java
+++ photo-service/src/main/java/kr/mafoo/photo/domain/PhotoEntity.java
@@ -62,6 +62,11 @@ public class PhotoEntity implements Persistable<String> {
         return photoId;
     }
 
+    public PhotoEntity updateOwnerMemberId(String ownerMemberId) {
+        this.ownerMemberId = ownerMemberId;
+        return this;
+    }
+
     public PhotoEntity updateAlbumId(String albumId) {
         this.albumId = albumId;
         return this;
