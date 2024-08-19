c168764886c0f1e974c74fad81aa71c51beb368d
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-19 23:20:00 +0900

    refactor: add schema example for PhotoBulkUpdateAlbumIdRequest

--- photo-service/src/main/java/kr/mafoo/photo/controller/dto/request/PhotoBulkUpdateAlbumIdRequest.java
+++ photo-service/src/main/java/kr/mafoo/photo/controller/dto/request/PhotoBulkUpdateAlbumIdRequest.java
@@ -1,12 +1,16 @@
 package kr.mafoo.photo.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import kr.mafoo.photo.annotation.ULID;
 
 @Schema(description = "사진 앨범 수정 요청")
 public record PhotoBulkUpdateAlbumIdRequest(
 
-        @Schema(description = "사진 ID 목록")
+        @ArraySchema(
+                schema = @Schema(description = "사진 ID 목록"),
+                arraySchema = @Schema(example = "[\"test_photo_id_1\", \"test_photo_id_2\", \"test_photo_id_3\"]")
+        )
         String[] photoIds,
 
         @ULID
