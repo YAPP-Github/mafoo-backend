7def256cb1f010a93033a56ef4ea2168ff2a6cfe
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-25 06:25:02 +0900

    fix: fix db migration v8

--- photo-service/src/main/resources/db/migration/V8__createPermissionTable.sql
+++ photo-service/src/main/resources/db/migration/V8__createPermissionTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE permission(
     `id` CHAR(26) PRIMARY KEY NOT NULL COMMENT '권한아이디',
     `type` VARCHAR(255) NOT NULL COMMENT '권한종류',
     `member_id` CHAR(26) NOT NULL COMMENT '권한대상사용자아이디',
-    `album_id` CHAR(26) NULL COMMENT '권한대상앨범아이디',
+    `album_id` CHAR(26) NOT NULL COMMENT '권한대상앨범아이디',
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     INDEX `permission_idx1` (`member_id`),
