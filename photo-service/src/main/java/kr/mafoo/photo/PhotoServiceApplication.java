ed69bfd695b00ce89a19f49550c6846465bb55ec
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-31 12:21:05 +0900

    fix: add @EnableR2dbcAuditing

--- photo-service/src/main/java/kr/mafoo/photo/PhotoServiceApplication.java
+++ photo-service/src/main/java/kr/mafoo/photo/PhotoServiceApplication.java
@@ -2,7 +2,9 @@ package kr.mafoo.photo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
+@EnableR2dbcAuditing
 @SpringBootApplication
 public class PhotoServiceApplication {
 
