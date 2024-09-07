e07d54ef7e2e7986e754a326b911db8bc5d7f5a9
Gyoungmin Kim <kimkm0419@naver.com>
2024-09-07 20:12:09 +0900

    refactor: change handleExceptionInternal to send requestBody info

--- photo-service/src/main/java/kr/mafoo/photo/handler/GlobalExceptionHandler.java
+++ photo-service/src/main/java/kr/mafoo/photo/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import kr.mafoo.photo.service.SlackService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,8 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @ControllerAdvice
 @RequiredArgsConstructor
@@ -21,36 +24,70 @@ public class GlobalExceptionHandler {
     private final SlackService slackService;
 
     @ExceptionHandler(ResponseStatusException.class)
-    public Mono<ResponseEntity<String>> handleResponseStatusException(ServerWebExchange exchange, ResponseStatusException ex) {
-        return handleExceptionInternal(exchange, ex, (HttpStatus) ex.getStatusCode());
+    public Mono<ResponseEntity<String>> handleResponseStatusException(ServerWebExchange exchange, ResponseStatusException exception) {
+        return handleExceptionInternal(exchange, exception, (HttpStatus) exception.getStatusCode());
     }
 
     @ExceptionHandler(Exception.class)
-    public Mono<ResponseEntity<String>> handleGenericException(ServerWebExchange exchange, Exception ex) {
-        return handleExceptionInternal(exchange, ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    public Mono<ResponseEntity<String>> handleGenericException(ServerWebExchange exchange, Exception exception) {
+        return handleExceptionInternal(exchange, exception, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private Mono<ResponseEntity<String>> handleExceptionInternal(ServerWebExchange exchange, Exception ex, HttpStatus status) {
-        String method = exchange.getRequest().getMethod().toString();
-        String userAgent = exchange.getRequest().getHeaders().getFirst("User-Agent");
+    private Mono<ResponseEntity<String>> handleExceptionInternal(ServerWebExchange exchange, Exception exception, HttpStatus status) {
+        String method = extractMethod(exchange);
+        String userAgent = extractUserAgent(exchange);
+        String fullPath = extractFullPath(exchange);
+        String originIp = extractOriginIp(exchange);
+
+        return extractRequestBody(exchange).flatMap(requestBody -> {
+
+            logException(method, fullPath, originIp, userAgent, exception);
+
+            if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
+                return slackService.sendErrorNotification(
+                        method, fullPath, requestBody, originIp, userAgent, exception.getMessage()
+                ).then(Mono.just(new ResponseEntity<>("Internal Server Error", status)));
+            }
+
+            return Mono.just(new ResponseEntity<>(status.getReasonPhrase(), status));
+        });
+    }
+
+    private String extractMethod(ServerWebExchange exchange) {
+        return exchange.getRequest().getMethod().toString();
+    }
+
+    private String extractUserAgent(ServerWebExchange exchange) {
+        return exchange.getRequest().getHeaders().getFirst("User-Agent");
+    }
+
+    private String extractFullPath(ServerWebExchange exchange) {
+        var request = exchange.getRequest();
+        String fullPath = request.getURI().getRawPath();
+        String query = request.getURI().getQuery();
+        return (query != null && !query.isEmpty()) ? fullPath + "?" + query : fullPath;
+    }
+
+    private String extractOriginIp(ServerWebExchange exchange) {
         String proxyIp = exchange.getRequest().getHeaders().getFirst("X-Forwarded-For");
-        InetSocketAddress address = exchange.getRequest().getRemoteAddress();
-        String originIp = proxyIp != null ? proxyIp : (address != null ? address.toString() : "UNKNOWN SOURCE");
-        String fullPath = exchange.getRequest().getURI().getPath() +
-                (exchange.getRequest().getURI().getQuery() != null ? "?" + exchange.getRequest().getURI().getQuery() : "");
-
-        logger.error("Exception occurred: {} {} {} ERROR {} {}", method, fullPath, originIp, ex.getMessage(), userAgent);
-
-        if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
-            return slackService.sendErrorNotification(
-                    method,
-                    fullPath,
-                    originIp,
-                    userAgent,
-                    ex.getMessage()
-            ).then(Mono.just(new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR)));
-        }
-
-        return Mono.just(new ResponseEntity<>(status.getReasonPhrase(), status));
+        InetSocketAddress remoteAddress = exchange.getRequest().getRemoteAddress();
+        return Optional.ofNullable(proxyIp)
+                .orElseGet(() -> remoteAddress != null ? remoteAddress.getAddress().getHostAddress() : "UNKNOWN SOURCE");
     }
+
+    private Mono<String> extractRequestBody(ServerWebExchange exchange) {
+        return exchange.getRequest().getBody().map(dataBuffer -> {
+                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                    dataBuffer.read(bytes);
+                    DataBufferUtils.release(dataBuffer);
+
+                    return new String(bytes, StandardCharsets.UTF_8);
+                }).reduce(new StringBuilder(), StringBuilder::append)
+                .map(StringBuilder::toString);
+    }
+
+    private void logException(String method, String fullPath, String originIp, String userAgent, Exception exception) {
+        logger.error("Exception occurred: {} {} {} ERROR {} {}", method, fullPath, originIp, exception.getMessage(), userAgent);
+    }
+
 }
