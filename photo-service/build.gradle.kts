8d17980b8a6ca83f1a6adf2177563c369efc6182
Gyoungmin Kim <kimkm0419@naver.com>
2024-11-04 09:25:35 +0900

    fix: change docker base image name

--- photo-service/build.gradle.kts
+++ photo-service/build.gradle.kts
@@ -53,7 +53,7 @@ jib {
 	val imageTag: String? = System.getenv("IMAGE_TAG")
 	val serverPort: String = System.getenv("SERVER_PORT") ?: "8080"
 	from {
-		image = "spinachpasta/custom-base-image:latest"
+		image = "spinachpasta/photo-service-base:latest"
 	}
 	to {
 		image = imageName
