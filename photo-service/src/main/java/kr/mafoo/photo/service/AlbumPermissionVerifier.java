78f2de0b39f1682c0baf16b9a5593c16d9a6c9cc
Gyoungmin Kim <kimkm0419@naver.com>
2024-11-21 19:17:04 +0900

    fix: fix album permission verifier errors

--- photo-service/src/main/java/kr/mafoo/photo/service/AlbumPermissionVerifier.java
+++ photo-service/src/main/java/kr/mafoo/photo/service/AlbumPermissionVerifier.java
@@ -1,27 +1,36 @@
 package kr.mafoo.photo.service;
 
+import static kr.mafoo.photo.domain.enums.ShareStatus.ACCEPTED;
+
 import kr.mafoo.photo.domain.AlbumEntity;
 import kr.mafoo.photo.domain.enums.PermissionLevel;
+import kr.mafoo.photo.domain.enums.ShareStatus;
 import kr.mafoo.photo.exception.AlbumOwnerMismatchException;
+import kr.mafoo.photo.exception.SharedMemberNotFoundException;
 import kr.mafoo.photo.exception.SharedMemberPermissionDeniedException;
+import kr.mafoo.photo.exception.SharedMemberStatusNotAcceptedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
-public class AlbumPermissionQuery {
+public class AlbumPermissionVerifier {
 
     private final AlbumQuery albumQuery;
     private final SharedMemberQuery sharedMemberQuery;
 
     public Mono<AlbumEntity> verifyOwnershipOrAccessPermission(String albumId, String requestMemberId, PermissionLevel permissionLevel) {
         return verifyOwnership(albumId, requestMemberId)
-            .onErrorResume(AlbumOwnerMismatchException.class, ex ->
+            .onErrorResume(AlbumOwnerMismatchException.class, ownerEx ->
                 sharedMemberQuery.findByAlbumIdAndMemberId(albumId, requestMemberId)
+                    .onErrorResume(SharedMemberNotFoundException.class, sharedEx ->
+                        Mono.error(new SharedMemberPermissionDeniedException())
+                    )
                     .flatMap(sharedAlbumMember ->
-                        checkAccessPermission(sharedAlbumMember.getPermissionLevel(), permissionLevel)
-                    ).then(albumQuery.findById(albumId))    // TODO : findById 중복 실행 제거 필요
+                            checkShareStatus(sharedAlbumMember.getShareStatus())
+                                .then(checkAccessPermission(sharedAlbumMember.getPermissionLevel(), permissionLevel))
+                    ).then(albumQuery.findById(albumId))    // FIXME : findById 중복 실행 제거 필요
             );
     }
 
@@ -35,6 +44,13 @@ public class AlbumPermissionQuery {
     private Mono<Void> checkOwnership(String ownerMemberId, String requestMemberId) {
         if (!ownerMemberId.equals(requestMemberId)) {
             return Mono.error(new AlbumOwnerMismatchException());
+        }
+        return Mono.empty();
+    }
+
+    private Mono<Void> checkShareStatus(ShareStatus status) {
+        if (!status.equals(ACCEPTED)) {
+            return Mono.error(new SharedMemberStatusNotAcceptedException());
         }
         return Mono.empty();
     }
