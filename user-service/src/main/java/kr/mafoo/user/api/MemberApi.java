a445724eb98d356f4564b1d11451b2262d98a0a7
Gyoungmin Kim <kimkm0419@naver.com>
2024-11-21 21:47:29 +0900

    feat: implement member related apis

---
+++ user-service/src/main/java/kr/mafoo/user/api/MemberApi.java
@@ -0,0 +1,43 @@
+package kr.mafoo.user.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.mafoo.user.annotation.RequestMemberId;
+import kr.mafoo.user.controller.dto.response.MemberResponse;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Tag(name = "사용자 관련 API", description = "사용자 정보 조회 API")
+@Validated
+@RequestMapping("/v1/members")
+public interface MemberApi {
+    @Operation(summary = "사용자 검색", description = "키워드로 사용자를 검색합니다. (이름으로 검색)")
+    @GetMapping
+    Flux<MemberResponse> getMemberListByName(
+        @RequestMemberId
+        @Parameter(hidden = true)
+        String requesterId,
+
+        @Parameter(description = "검색어", example = "사람")
+        @RequestParam
+        String keyword
+    );
+
+    @Operation(summary = "사용자 단건 조회", description = "사용자 단건 정보를 조회합니다.")
+    @GetMapping("{memberId}")
+    Mono<MemberResponse> getMember(
+        @RequestMemberId
+        @Parameter(hidden = true)
+        String requesterId,
+
+        @Parameter(description = "사용자 ID", example = "test_member_id")
+        @PathVariable
+        String memberId
+    );
+}
--- user-service/src/main/java/kr/mafoo/user/repository/MemberRepository.java
+++ user-service/src/main/java/kr/mafoo/user/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package kr.mafoo.user.repository;
 
 import kr.mafoo.user.domain.MemberEntity;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface MemberRepository extends R2dbcRepository<MemberEntity, String> {
+    Flux<MemberEntity> findAllByNameContaining(String memberId);
     Mono<Void> deleteMemberById(String memberId);
 }
