d98d893f084cbeb33642201d35ed47a08446b18c
Gyoungmin Kim <kimkm0419@naver.com>
2024-12-01 10:47:58 +0900

    fix: fix dontlookup qr regex

--- photo-service/src/main/java/kr/mafoo/photo/domain/enums/BrandType.java
+++ photo-service/src/main/java/kr/mafoo/photo/domain/enums/BrandType.java
@@ -8,7 +8,7 @@ public enum BrandType {
     LIFE_FOUR_CUTS(Pattern.compile("https://api\\.life4cut\\.net/.*")),
     PHOTOISM(Pattern.compile("https://qr\\.seobuk\\.kr/.*")),
     HARU_FILM(Pattern.compile("http://haru\\d+\\.mx\\d+\\.co\\.kr/.*")),
-    DONT_LOOK_UP(Pattern.compile("https://x\\.dontlxxkup\\.kr/.*")),
+    DONT_LOOK_UP(Pattern.compile("https://\\w+\\.dontlxxkup\\.kr/.*")),
     MY_FOUR_CUT(Pattern.compile("https://firebasestorage\\.googleapis\\.com:443/v0/b/my4ccu\\.appspot\\.com/.*")),
     PHOTOGRAY(Pattern.compile("https://pgshort\\.aprd\\.io/.*")),
     MONOMANSION(Pattern.compile("https://monomansion\\.net/.*")),
