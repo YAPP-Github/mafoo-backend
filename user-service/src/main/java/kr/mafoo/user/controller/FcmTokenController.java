3a63db0f2bb2d4bd3a0b552aea29182f4528590b
Gyoungmin Kim <kimkm0419@naver.com>
2025-02-21 09:48:30 +0900

    feat: add notification sending logic for NEW_MEMBER scenario

--- user-service/src/main/java/kr/mafoo/user/controller/FcmTokenController.java
+++ user-service/src/main/java/kr/mafoo/user/controller/FcmTokenController.java
@@ -1,10 +1,14 @@
 package kr.mafoo.user.controller;
 
+import static kr.mafoo.user.enums.NotificationType.NEW_MEMBER;
+
+import java.util.List;
 import kr.mafoo.user.api.FcmTokenApi;
 import kr.mafoo.user.controller.dto.request.FcmTokenCreateRequest;
 import kr.mafoo.user.controller.dto.request.FcmTokenUpdateRequest;
 import kr.mafoo.user.controller.dto.response.FcmTokenResponse;
 import kr.mafoo.user.service.FcmTokenService;
+import kr.mafoo.user.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,6 +19,8 @@ import reactor.core.publisher.Mono;
 public class FcmTokenController implements FcmTokenApi {
 
     private final FcmTokenService fcmTokenService;
+
+    private final NotificationService notificationService;
 
     @Override
     public Flux<FcmTokenResponse> getFcmTokenList(
@@ -35,7 +41,10 @@ public class FcmTokenController implements FcmTokenApi {
                 memberId,
                 request.token()
             )
-            .map(FcmTokenResponse::fromEntity);
+            .map(FcmTokenResponse::fromEntity)
+            .flatMap(fcmTokenResponse -> notificationService.sendNotificationByScenario(NEW_MEMBER, List.of(memberId), null)
+                .then(Mono.just(fcmTokenResponse))
+            );
     }
 
     @Override
