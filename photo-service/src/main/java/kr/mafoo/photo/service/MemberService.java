7cffe161ea7f1c458dbe795bc32264f08ee918a7
Gyoungmin Kim <kimkm0419@naver.com>
2025-02-24 16:52:46 +0900

    feat: implement send scenario notification webclient logic

--- photo-service/src/main/java/kr/mafoo/photo/service/MemberService.java
+++ photo-service/src/main/java/kr/mafoo/photo/service/MemberService.java
@@ -1,7 +1,12 @@
 package kr.mafoo.photo.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import kr.mafoo.photo.domain.enums.NotificationType;
 import kr.mafoo.photo.service.dto.MemberDto;
 import kr.mafoo.photo.exception.MafooUserApiFailedException;
+import kr.mafoo.photo.service.dto.NotificationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,28 @@ public class MemberService {
     private String endpoint;
 
     private final WebClient client;
+
+    public Mono<NotificationDto> sendScenarioNotification(NotificationType notificationType, List<String> receiverMemberIds, Map<String, String> variables) {
+        return client
+            .post()
+            .uri(endpoint + "/user/v1/notifications/")
+            .bodyValue(
+                createSendNotificationRequestBody(notificationType, receiverMemberIds, variables)
+            )
+            .retrieve()
+            .onStatus(status -> !status.is2xxSuccessful(), (res) -> Mono.error(new MafooUserApiFailedException()))
+            .bodyToMono(NotificationDto.class);
+    }
+
+    private Map<String, Object> createSendNotificationRequestBody(NotificationType notificationType, List<String> receiverMemberIds, Map<String, String> variables) {
+        Map<String, Object> requestBody = new HashMap<>();
+
+        requestBody.put("notificationType", notificationType);
+        requestBody.put("receiverMemberIds", receiverMemberIds);
+        requestBody.put("variables", variables);
+
+        return requestBody;
+    }
 
     public Mono<MemberDto> getMemberInfoById(String memberId) {
         return client
