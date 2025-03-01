04b802d5f2b530f02909b9fca824250f4cf6d174
Gyoungmin Kim <kimkm0419@naver.com>
2025-03-02 07:32:19 +0900

    fix: fix NEW_MEMBER notification variables field

--- user-service/src/main/java/kr/mafoo/user/service/FcmTokenService.java
+++ user-service/src/main/java/kr/mafoo/user/service/FcmTokenService.java
@@ -37,7 +37,7 @@ public class FcmTokenService {
         return memberQuery.findById(requestMemberId)
             .flatMap(member -> fcmTokenQuery.checkDuplicateExists(requestMemberId)
                     .then(fcmTokenCommand.addFcmToken(requestMemberId, token)
-                        .flatMap(fcmToken -> notificationService.sendNotificationByScenario(NEW_MEMBER, List.of(requestMemberId), Map.of("receiverName", member.getName()))
+                        .flatMap(fcmToken -> notificationService.sendNotificationByScenario(NEW_MEMBER, List.of(requestMemberId), null)
                             .then(Mono.just(fcmToken))
                         )
                     )
