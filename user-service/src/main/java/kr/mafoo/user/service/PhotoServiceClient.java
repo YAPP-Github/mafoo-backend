1cde0062ba7f8f33dd2b16c904e32058f4eb49ce
Gyoungmin Kim <kimkm0419@naver.com>
2025-03-04 22:06:15 +0900

    fix: fix typo

--- user-service/src/main/java/kr/mafoo/user/service/PhotoServiceClient.java
+++ user-service/src/main/java/kr/mafoo/user/service/PhotoServiceClient.java
@@ -43,7 +43,7 @@ public class PhotoServiceClient {
     public Flux<SharedMemberDto> getSharedMemberFluxByAlbumId(String albumId, List<String> memberIdList, String authorizationToken) {
         return client
             .get()
-            .uri(gatewayEndpoint + "photo/v1/shared-members?albumId=" + albumId + "&memberIdList=" + String.join(",", memberIdList))
+            .uri(gatewayEndpoint + "/photo/v1/shared-members?albumId=" + albumId + "&memberIdList=" + String.join(",", memberIdList))
             .header("Authorization", "Bearer " + authorizationToken)
             .retrieve()
             .onStatus(status -> status.isSameCodeAs(HttpStatus.BAD_REQUEST), (res) -> Mono.empty())
